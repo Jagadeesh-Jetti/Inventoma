dba8a29b3ee1d2faf1430939bc40ba8100f4795d
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-12-09 22:12:21 +0530

    feat: added routes for navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 
+import { Route, Routes } from 'react-router-dom';
+import { Navbar } from './components/Navbar/navbar';
+import { Inventories } from './pages/Inventories/inventories';
+import { Sales } from './pages/Sales/sales';
+import { Reports } from './pages/Reports/reports';
+import { Home } from './pages/Home/home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1> hey ooo</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home /> } />
+        <Route path="inventories" element={<Inventories />} />
+        <Route path="sales" element={<Sales />} />
+        <Route path="reports" element={<Reports />} />
+      </Routes>
     </div>
   );
 }
