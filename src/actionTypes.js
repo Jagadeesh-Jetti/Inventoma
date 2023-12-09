460bb76d7331b7437dc84f4c70d265af19f24e18
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-12-09 22:11:57 +0530

    feat: added action types

---
+++ src/actionTypes.js
@@ -0,0 +1,5 @@
+export const FETCH_INVENTORIES = "fetchInventories"
+export const FETCH_SALES = "fetchSales"
+export const ADD_DATA = "addData"
+export const UPDATE_DATA = "updateData"
+export const DELETE_INVENTORY = "deleteInventory"
