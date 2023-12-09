b6b9cb2136eff901759de742a181909143acad97
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-12-09 22:17:01 +0530

    feat: had setup basic sales page

---
+++ src/pages/Sales/sales.jsx
@@ -0,0 +1,8 @@
+export const Sales = () => {
+  return (
+    <div className="sales_main">
+      <div className="sales_name">Sales</div>
+      <div className="sales_layout"></div>
+    </div>
+  );
+};
