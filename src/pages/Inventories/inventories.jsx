90bfdc5d668b80d9f07fe9eb9799715dcf49b6a7
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-12-09 22:15:52 +0530

    feat: added inventories display

---
+++ src/pages/Inventories/inventories.jsx
@@ -0,0 +1,37 @@
+import { useDispatch, useSelector } from "react-redux";
+import "../Inventories/inventories.css";
+import { GetInventories } from "../../actions";
+import { useEffect } from "react";
+
+export const Inventories = () => {
+  const inventories = useSelector((state) => state.inventories);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(GetInventories());
+  }, [dispatch]);
+
+  return (
+    <div className="invent_main">
+      <div className="invent_name">Inventories</div>
+      <div className="invent_layout">
+        {inventories.map((inventory) => (
+          <div className="invent_card">
+            <p>
+              <b> Name: </b> {inventory.name}
+            </p>
+            <p>
+              <b> Quantity: </b> {inventory.quantity}
+            </p>
+            <p>
+              <b> Data: </b> {new Date(inventory?.createdAt).toLocaleString()}
+            </p>
+            <p>
+              <b> Price: </b> {inventory.price}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
