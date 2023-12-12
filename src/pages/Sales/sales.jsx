f1b52ce2ebc27e8f0f3a8b493725b6e9ab5008e6
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-12-12 20:25:27 +0530

    feat: added the sales display tables

--- src/pages/Sales/sales.jsx
+++ src/pages/Sales/sales.jsx
@@ -1,8 +1,42 @@
+import React from "react";
+import { useEffect } from "react";
+import { GetSales } from "../../actions";
+import { useDispatch, useSelector } from "react-redux";
+import "../Sales/sales.css";
+
 export const Sales = () => {
+  const dispatch = useDispatch();
+  const sales = useSelector((state) => state.sales);
+
+  useEffect(() => {
+    dispatch(GetSales());
+  }, [dispatch]);
+
   return (
     <div className="sales_main">
       <div className="sales_name">Sales</div>
-      <div className="sales_layout"></div>
+      <div className="sales_layout">
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Quantity</th>
+              <th>Data</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sales.map((sale, index) => (
+              <tr key={index}>
+                <td>{sale.name}</td>
+                <td>{sale.quantity}</td>
+                <td>{new Date(sale.createdAt).toLocaleDateString("en-GB")}</td>
+                <td>{sale.price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
