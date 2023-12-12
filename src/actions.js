e2a67e62ded63a7cd65392959fab0de942e66386
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-12-12 20:24:51 +0530

    feat: added getSales action creator

--- src/actions.js
+++ src/actions.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { FETCH_INVENTORIES } from "./actionTypes";
+import { FETCH_INVENTORIES, FETCH_SALES } from "./actionTypes";
+import { Inventories } from "./pages/Inventories/inventories";
 
 const url = "https://assignment18.vishalsoni7.repl.co"
 
@@ -15,5 +16,19 @@ export const GetInventories = () => async (dispatch) => {
     }
     catch(error){
         console.error("Error while fetching inventories: ", error)
+    }
+}
+
+export const GetSales = () => async (dispatch) => {
+    try{
+        setTimeout(async () => {
+            const {
+                data: {data}
+            } = await axios.get(`${url}/sales`)
+            dispatch({ type: FETCH_SALES, payload: data})
+        }, 500);
+    }
+    catch(error){
+        console.error("Error while fetching sales: ", error)
     }
 }
