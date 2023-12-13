a13bacc44e1cbc1ce3d51c6f1a83d3aa15eb917e
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-12-13 23:20:31 +0530

    feat: added ADD_DATA, DELETE_INVENTORY cases

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,4 @@
-import { FETCH_INVENTORIES, FETCH_SALES } from "./actionTypes"
+import { ADD_DATA, DELETE_INVENTORY, FETCH_INVENTORIES, FETCH_SALES } from "./actionTypes"
 
 export const initialState ={
     inventories: [],
@@ -19,6 +19,26 @@ export const reducer = (state = initialState, action) => {
         return{
           ...state,
           sales: payload
+        }
+      }
+      case ADD_DATA: {
+        if(payload.type === "inventories"){
+          return{
+            ...state,
+            inventories: [...state.inventories, payload]
+          }
+        }
+        else{
+          return{
+            ...state,
+            sales: [...state.sales, payload]
+          }
+        }
+      }
+      case DELETE_INVENTORY: {
+        return{
+          ...state,
+          inventories: state.inventories.filter((inventory) => inventory._id !== payload._id)
         }
       }
       default:
