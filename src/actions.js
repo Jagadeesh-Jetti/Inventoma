0426b4a68aa0bd20148f70c4c2027f41ee601e8a
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-02-21 13:36:54 +0530

    feat: updated the backend url and methods

--- src/actions.js
+++ src/actions.js
@@ -1,70 +1,61 @@
 import axios from "axios";
-import { ADD_DATA, DELETE_INVENTORY, FETCH_INVENTORIES, FETCH_SALES } from "./actionTypes";
+import {
+  ADD_DATA,
+  DELETE_INVENTORY,
+  FETCH_INVENTORIES,
+  FETCH_SALES,
+} from "./actionTypes";
 
-const url = "https://assignment18.vishalsoni7.repl.co"
+const url = "https://inventory-management-backend-six.vercel.app";
 
 export const GetInventories = () => async (dispatch) => {
-    try{
-        setTimeout(async () => {
-            const {
-                data: {data}
-            } = await axios.get(`${url}/inventories`)
-            dispatch({ type: FETCH_INVENTORIES, payload: data})
-        }, 500)
-    }
-    catch(error){
-        console.error("Error while fetching inventories: ", error)
-    }
-}
+  try {
+    const { data } = await axios.get(`${url}/inventories`);
+    dispatch({ type: FETCH_INVENTORIES, payload: data });
+  } catch (error) {
+    console.error("Error while fetching inventories: ", error);
+  }
+};
 
 export const GetSales = () => async (dispatch) => {
-    try{
-        setTimeout(async () => {
-            const {
-                data: {data}
-            } = await axios.get(`${url}/sales`)
-            dispatch({ type: FETCH_SALES, payload: data})
-        }, 500);
-    }
-    catch(error){
-        console.error("Error while fetching sales: ", error)
-    }
-}
+  try {
+    const { data } = await axios.get(`${url}/sales`);
+    dispatch({ type: FETCH_SALES, payload: data });
+  } catch (error) {
+    console.error("Error while fetching sales: ", error);
+  }
+};
 
 export const AddEntry = (entryDetails) => async (dispatch) => {
-    try{
-        let completeUrl;
-        
-        if(entryDetails.type === "inventories"){
-            completeUrl = `${url}/inventories`
-        }
-        else{
-            completeUrl = `${url}/sales`
-        }
+  try {
+    let completeUrl;
 
-        const { 
-            data : { data } 
-        } = await axios.post(completeUrl, entryDetails, {
-            headers: {
-                "Content-Type" : "application/json"
-            }
-        })
-        console.log(data)
-        dispatch({ type: ADD_DATA, payload: data})
-    } 
-    catch(error){
-        console.log("Error while adding entry to the database:", error)
+    if (entryDetails.type === "inventories") {
+      completeUrl = `${url}/inventories`;
+    } else {
+      completeUrl = `${url}/sales`;
     }
-}
+
+    const response = await axios.post(completeUrl, entryDetails, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    dispatch({ type: ADD_DATA, payload: response });
+  } catch (error) {
+    console.error("Error while adding entry to the database:", error);
+  }
+};
 
 export const DeleteEntry = (id) => async (dispatch) => {
-    try{
-        const { 
-            data : {data} 
-        } = await axios.delete(`${url}/inventories/${id}`);
-        dispatch({ type: DELETE_INVENTORY, payload: data})
-    }
-    catch(error){
-        console.log("Error while deleting the entry:", error)
-    }
-}
+  try {
+    const {
+      data: { data },
+    } = await axios.delete(`${url}/inventories/${id}`);
+    console.log(data);
+    dispatch({ type: DELETE_INVENTORY, payload: data });
+  } catch (error) {
+    console.error("Error while deleting the entry:", error);
+  }
+};
