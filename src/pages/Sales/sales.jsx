9fa35acf8fabfbbd0b6664f9065e0944552f2ac3
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-02-21 13:43:24 +0530

    refactor: formatted the table

--- src/pages/Sales/sales.jsx
+++ src/pages/Sales/sales.jsx
@@ -13,25 +13,25 @@ export const Sales = () => {
   }, [dispatch]);
 
   return (
-    <div className="sales_main">
-      <div className="sales_name">Sales</div>
-      <div className="sales_layout">
+    <div className="sales-main">
+      <div className="sales-name">Sales</div>
+      <div className="sales-layout">
         <table>
           <thead>
             <tr>
               <th>Name</th>
-              <th>Quantity</th>
-              <th>Data</th>
               <th>Price</th>
+              <th>Quantity</th>
+              <th>Date</th>
             </tr>
           </thead>
           <tbody>
-            {sales.map((sale, index) => (
+            {sales?.map((sale, index) => (
               <tr key={index}>
                 <td>{sale.name}</td>
+                <td>{sale.price}</td>
                 <td>{sale.quantity}</td>
                 <td>{new Date(sale.createdAt).toLocaleDateString("en-GB")}</td>
-                <td>{sale.price}</td>
               </tr>
             ))}
           </tbody>
