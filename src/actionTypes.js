f91f8005a1581d1fed8bc127032ad36c08a4ee77
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-02-21 13:37:37 +0530

    fix: updated action types

--- src/actionTypes.js
+++ src/actionTypes.js
@@ -1,5 +1,5 @@
-export const FETCH_INVENTORIES = "fetchInventories"
-export const FETCH_SALES = "fetchSales"
-export const ADD_DATA = "addData"
-export const UPDATE_DATA = "updateData"
-export const DELETE_INVENTORY = "deleteInventory"
+export const FETCH_INVENTORIES = "fetchInventories";
+export const FETCH_SALES = "fetchSales";
+export const ADD_DATA = "addData";
+export const UPDATE_DATA = "updateData";
+export const DELETE_INVENTORY = "deleteInventory";
