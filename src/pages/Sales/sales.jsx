661b7a79fb15427c6b5a452de796007e360919c5
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-03-01 23:00:04 +0530

    feat: added data filter

--- src/pages/Sales/sales.jsx
+++ src/pages/Sales/sales.jsx
@@ -1,20 +1,46 @@
-import React from "react";
-import { useEffect } from "react";
-import { GetSales } from "../../actions";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { GetSales } from "../../actions";
 import "../Sales/sales.css";
 
 export const Sales = () => {
   const dispatch = useDispatch();
-  const sales = useSelector((state) => state.sales);
+  const allSales = useSelector((state) => state.sales);
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+
+  const filteredSales = allSales.filter((sale) => {
+    const saleDate = new Date(sale.createdAt).getTime();
+    const startTimestamp = startDate ? new Date(startDate).getTime() : 0;
+    const endTimestamp = endDate
+      ? new Date(endDate).getTime()
+      : Number.MAX_SAFE_INTEGER;
+
+    return saleDate >= startTimestamp && saleDate <= endTimestamp;
+  });
 
   useEffect(() => {
+    // Fetch all sales data
     dispatch(GetSales());
   }, [dispatch]);
 
+  const handleStartDateChange = (event) => {
+    setStartDate(event.target.value);
+  };
+
+  const handleEndDateChange = (event) => {
+    setEndDate(event.target.value);
+  };
+
   return (
     <div className="sales-main">
-      <div className="sales-name">Sales</div>
+      <div className="sales-name">Sales History</div>
+      <div className="date-filter">
+        <label>Start Date:</label>
+        <input type="date" value={startDate} onChange={handleStartDateChange} />
+        <label>End Date:</label>
+        <input type="date" value={endDate} onChange={handleEndDateChange} />
+      </div>
       <div className="sales-layout">
         <table>
           <thead>
@@ -26,7 +52,7 @@ export const Sales = () => {
             </tr>
           </thead>
           <tbody>
-            {sales?.map((sale, index) => (
+            {filteredSales.map((sale, index) => (
               <tr key={index}>
                 <td>{sale.name}</td>
                 <td>{sale.price}</td>
